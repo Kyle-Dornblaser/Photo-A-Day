035adb071d37ae07e59f5fd8ecc619fe71d9da50
Kyle <kdornblaser@gmail.com>
2016-12-06 19:17:17 -0500

    Refactor some code to method and rename files to numbers.

--- program.py
+++ program.py
@@ -63,33 +63,54 @@ for imageName in images:
         skipped += 1
 print "Could not find faces in {0} photos".format(skipped)
 
-for face in facesList:
-    scaleFactor = face.scaleFactor(750 * 750)
-    image = cv2.imread(face.path)
-    (height, width) = image.shape[:2]
-    newDim = (int(width * scaleFactor), int(height * scaleFactor))
+def resizeAndCrop():
+    for face in facesList:
+        scaleFactor = face.scaleFactor(750 * 750)
+        image = cv2.imread(face.path)
+        (height, width) = image.shape[:2]
+        newDim = (int(width * scaleFactor), int(height * scaleFactor))
 
-    resized = cv2.resize(image, newDim, interpolation = cv2.INTER_AREA)
+        resized = cv2.resize(image, newDim, interpolation = cv2.INTER_AREA)
 
-    resizedFaceY = int(face.y * scaleFactor)
-    resizedFaceX = int(face.x * scaleFactor)
-    resizedFaceH = int(face.h * scaleFactor)
-    resizedFaceW = int(face.w * scaleFactor)
-    padding = 100
-    topBorder = resizedFaceY - (1080 / 2) + resizedFaceH / 2
-    bottomBorder = topBorder + (1080)
-    leftBorder = resizedFaceX - (1920 / 2) + resizedFaceW / 2
-    rightBorder = leftBorder + (1920)
-    print "Name: {0} \t Scale Factor: {1}".format(face.path[-10:], scaleFactor)
-    if (leftBorder < 0 or bottomBorder < 0):
-        print "skipping {0}".format(face.path)
-        os.remove(face.path)
-    else:
-        cropped = resized[topBorder:bottomBorder, leftBorder:rightBorder]
-        cv2.imwrite(face.path, cropped)
+        resizedFaceY = int(face.y * scaleFactor)
+        resizedFaceX = int(face.x * scaleFactor)
+        resizedFaceH = int(face.h * scaleFactor)
+        resizedFaceW = int(face.w * scaleFactor)
+        padding = 100
+        topBorder = resizedFaceY - (1080 / 2) + resizedFaceH / 2
+        bottomBorder = topBorder + (1080)
+        leftBorder = resizedFaceX - (1920 / 2) + resizedFaceW / 2
+        rightBorder = leftBorder + (1920)
+        print "Name: {0} \t Scale Factor: {1}".format(face.path[-10:], scaleFactor)
+        if (leftBorder < 0 or bottomBorder < 0):
+            print "skipping {0}".format(face.path)
+            os.remove(face.path)
+        else:
+            cropped = resized[topBorder:bottomBorder, leftBorder:rightBorder]
+            cv2.imwrite(face.path, cropped)
 
-#def renameAsNumbers(directory):
-    # TODO rename files in given directory 001, 002, etc.
+# necessary for outputting to video
+def renameAsNumbers(directory):
+    images = os.listdir(directory)
+    images.sort()
+    numImages = len(images)
+    numImagesStrLen = len("{0}".format(numImages))
+    counter = 1
+    for image in images:
+        filetype = image[-4:]
+        newName = "{0}".format(counter)
+        while (len(newName) < numImagesStrLen):
+            newName = "0" + newName
+        os.rename(directory + image, "{0}{1}{2}".format(directory, newName, filetype))
+        counter += 1
+
+
+resizeAndCrop()
+renameAsNumbers(subdirectory)
+
+
+
+
 
 #cleanup
 #shutil.rmtree(subdirectory)
